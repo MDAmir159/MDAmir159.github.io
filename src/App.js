7fa1f68b16288acd3994c6fdb1d10bfb936fcec9
MDAmir159 <islamamirul497@gmail.com>
2022-08-25 16:15:44 +0600

    deploy commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.css';
+import PortFolio from './PortFolio';
+
+function App() {
+  return (
+    <PortFolio />
+  );
+}
+
+export default App;
---
+++ src/assets/contactInfo.js
@@ -0,0 +1,6 @@
+export const contactDetails = {
+    address : "768, Munnafer Mor, Raninagar, Rajshahi",
+    country: "Bangladesh",
+    contactNo : "01306229664",
+    email : "islamamirul497@gmail.com"
+} 
